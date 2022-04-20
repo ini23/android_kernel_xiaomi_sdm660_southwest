31bcdb4d7abae4d04a81dfe30efc6c833ff5c4fb
Ubuntu <AzureUser@BetaTester01.jauathwh5hlujlmrzfrmfeb54e.lx.internal.cloudapp.net>
2022-04-20 22:29:43 +0700

    pp

--- drivers/input/touchscreen/synaptics_dsx_e7s/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_e7s/synaptics_dsx_core.c
@@ -922,7 +922,6 @@ static ssize_t synaptics_rmi4_wake_gesture_store(struct device *dev,
 	if (sscanf(buf, "%u", &input) != 1)
 		return -EINVAL;
 
-	input_event(rmi4_data->input_dev, EV_SYN, SYN_CONFIG, input ? WAKEUP_ON : WAKEUP_OFF);
 
 	input = input > 0 ? 1 : 0;
 
