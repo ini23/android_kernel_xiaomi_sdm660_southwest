299c7c43e17b6952f27726d8ca29d0c62551a5bd
iqbal <mi460334@gmail.com>
2022-07-01 02:55:43 +0000

    Revert "sync: Don't debug everything by default"
    This reverts commit 8425a75134771a4c73e5010c0b6054c075f17815.

--- drivers/staging/android/sync.h
+++ drivers/staging/android/sync.h
@@ -335,20 +335,21 @@ int sync_fence_cancel_async(struct sync_fence *fence,
  */
 int sync_fence_wait(struct sync_fence *fence, long timeout);
 
-#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_DEBUG_TIMELINE)
+#ifdef CONFIG_DEBUG_FS
+
 void sync_timeline_debug_add(struct sync_timeline *obj);
 void sync_timeline_debug_remove(struct sync_timeline *obj);
 void sync_fence_debug_add(struct sync_fence *fence);
 void sync_fence_debug_remove(struct sync_fence *fence);
 void sync_dump(void);
-#else
-static void inline sync_timeline_debug_add(struct sync_timeline *obj) {}
-static void inline sync_timeline_debug_remove(struct sync_timeline *obj) {}
-static void inline sync_fence_debug_add(struct sync_fence *fence) {}
-static void inline sync_fence_debug_remove(struct sync_fence *fence) {}
-static void inline sync_dump(void) {}
-#endif
 
+#else
+# define sync_timeline_debug_add(obj)
+# define sync_timeline_debug_remove(obj)
+# define sync_fence_debug_add(fence)
+# define sync_fence_debug_remove(fence)
+# define sync_dump()
+#endif
 int sync_fence_wake_up_wq(wait_queue_t *curr, unsigned mode,
 				 int wake_flags, void *key);
 
